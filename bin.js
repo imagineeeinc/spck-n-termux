207601acee55ba656742507096be1db816633d7b
Imagineee <imagineeelego@gmail.com>
2021-04-07 19:46:44 +0100

    added pull function

--- bin.js
+++ bin.js
@@ -6,15 +6,53 @@ const argv = require('yargs/yargs')(process.argv.slice(2)).argv
 const path = require('path');
 const fs = require('fs');
 const fse = require('fs-extra');
+const {settings} = require('cluster');
+
+var opts = {
+    speck_project_name:"helloworld"
+}
 
 if (argv._.indexOf("init") > -1) {
-} else if (argv._.indexOf("build") > -1 || argv._.indexOf("bake") > -1 || argv._.indexOf("b") > -1) {
-    console.log(chalk.green.bold("Starting Build Process"))
-    startbuild()
-} else if (argv._.indexOf("help") > -1) {
-    help()
+init(argv._[1])
+} else if (argv._.indexOf("pull") > -1) {
+    pull()
 } else if (argv._.indexOf("ver") > -1) {
     console.log(chalk.bold.green("version: " + ver))
 } else {
     help()
+}
+
+function init(name) {
+var setings = opts
+setting.speck_project_name = name
+fs.writeFile('spckntermux.json', JSON.stringify(settings), function (err) {
+  if (err) return console.log(err);
+});
+}
+function pull() {
+var opt = get_from_json()
+    fse.copySync(path.join("./storage/shared/Android/data/io.spck/files/" + opt.speck_project_name, process.cwd() + "/"), { overwrite: true }, function (err) {
+        if (err) {
+            console.log(chalk.red(err))
+        } else {
+            //console.log(chalk.green.bold("Done Setup!"))
+        }
+    });
+}
+
+function get_from_json() {
+var dir = path.join(process.cwd() + "/spckntermux.json");
+        var obj
+        try {
+            var data = fs.readFileSync(dir, 'utf8');
+            obj = data
+        } catch(e) {
+            console.log(chalk.red.bold('Error:', e.stack));
+        }
+        try {
+            obj = JSON.parse(obj)
+        } catch(err) {
+            console.error(err)
+        }
+    return obj
 }
